26b92da626dd922f611cddeeb96d0af6ce74fcb5
Sanika <137403800+sanikaP16@users.noreply.github.com>
2024-11-22 14:10:56 +0530

    Update rock_paper_scissor.js

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -1,62 +1,86 @@
 let userScore = 0;
-  let computerScore = 0;
+let computerScore = 0;
 
 function getComputerInput() {
   return Math.floor(Math.random() * 3);
 }
 
-function layout() {
-  console.log(" ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯");
-  console.log("| Your Turn | Computer | Your Score | Computer Score |");
-  console.log(" ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯");
-}
-
-function print(userTurn, computerTurn) {
-  console.log("|\t" + userTurn + "   | \t" + computerTurn + "\t|      " + userScore + "     |      " + computerScore + "        |");
-  console.log(" ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯");
-}
-
-function getComputerScore() {
-  return computerScore +1;
-}
-
-function getScore(userTurn, computerTurn) {
-  if (userTurn === computerTurn) {
-    console.log("Ohh! It's a tie!! Try again.");
-  } else if ((userTurn === 0 && computerTurn === 2) ||
-             (userTurn === 1 && computerTurn === 0) || 
-             (userTurn === 2 && computerTurn === 1)) {
-    userScore++;
-    console.log("You win this round!");
-  } else {
-    computerScore++;
-    console.log("Computer wins this round!");
-  }
-}
-
-function getUserInput() {
-  for(let i = 0; i < 3; i++) {
-  console.log("\n0 --> ✊🏻 (rock) \n1 --> 📃 (paper) \n2 --> ✂️  (scissors)");
-
-  //layout();
-    const userTurn = prompt("It's your turn! what is your choice ? ")
+function printLine(length) {
+  let line = "";
   
-    const computerTurn = getComputerInput();
-    getScore(userTurn, computerTurn)
-    //print(userTurn, computerTurn);
+  for (let i = 0; i < length; i++) {
+    line += "━";
   }
 
+  return line;
+}
+
+function whoWins(userTurn, computerTurn) {
+  const condition1 = userTurn === 0 && computerTurn === 2;
+  const condition2 = userTurn === 1 && computerTurn === 0;
+  const condition3 = userTurn === 2 && computerTurn === 1;
+
+  return condition1 || condition2 || condition3;
+}
+
+function getScore(nameOfPlayer, userTurn, computerTurn) {
+  if (userTurn === computerTurn) {
+    return "Ohh! It's a tie!! Try again.";
+  }
+
+  if (whoWins(userTurn, computerTurn)) {
+    return nameOfPlayer + " win!!!!";
+  }
+
+  return "Computer Wins!!!";
+}
+
+function discription() {
+  let discriptionMsg = "Here you will play with a computer.";
+  discriptionMsg += "\nYou can leave whenever you want ";
+  discription += "Your Choices : "
+  const signs = "\n0 --> ✊🏻 (rock) \n1 --> 📃 (paper) \n2 --> ✂️  (scissors)";
+
+  return discriptionMsg + signs;
+}
+
+function getInput() {
+  const userTurn = +prompt("\nIt's your turn! what is your choice ? ")
+
+  if (userTurn < 0 || userTurn > 2) {
+    console.log("\nInvalid Choice❗️ Please Enter a valid Choice!!")
+    return getInput();
+  }
+
+  return userTurn;
+}
+
+function getUserInput(nameOfPlayer) {
+  console.log(discription());
+
+  const userTurn = getInput();
+  const computerTurn = getComputerInput();
+
+  console.log("\n" + nameOfPlayer + "'s Choice : ", userTurn);
+  console.log("Computer's Choice : ", computerTurn);
+
+  console.log(getScore(nameOfPlayer, userTurn, computerTurn));
+}
+
+function getName() {
+  const nameOfPlayer = prompt("Enter your name :");
+  return nameOfPlayer;
 }
 
 function startGame() {
-  const line = "\n---------------------------------------------------------\n";
-  const welcomeMsg = "\n🔶🔶🔶🔶 Welcome to ROCK PAPER SCISSOR !!!! 🔶🔶🔶🔶\n"
-  let contextMessage = "\n  RULES : \n🟠 Game is played in two players ";
-  contextMessage += "\n🟠 The game will have 3 chance";
+  const welcomeMsg = "\n🔶🔶🔶🔶 Welcome to ROCK PAPER SCISSOR !!!! 🔶🔶🔶🔶\n";
+  console.log(printLine(60) + welcomeMsg + printLine(60));
+  const nameOfPlayer = getName();
 
-  console.log(line + welcomeMsg + contextMessage + line);
-  prompt("Press 'Enter' to play!!!");
-  getUserInput();
+  prompt(nameOfPlayer + " press 'Enter' to play!!!");
+  console.clear();
+  return getUserInput(nameOfPlayer);
+
 }
 
 startGame();
